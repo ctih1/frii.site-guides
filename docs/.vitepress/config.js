9f90808d9762c4e26ddd9f175508824227237789
ctih1 <nevalaonni@gmail.com>
2025-06-26 10:59:36 +0300

    added guide on how to reset password

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -31,7 +31,8 @@ export default {
                     { text: "Specifying ports", link: "/faq/ports" },
                     { text: "Increasing domain limit", link: "/faq/domain-limit" },
                     { text: "I forgot my username", link: "/faq/forgot-username"},
-                    { text: "How to support us", link: "/faq/supporting"}
+                    { text: "How to support us", link: "/faq/supporting"},
+                    { text: "Changing password", link: "/faq/changing-password"}
                 ],
             },
             {
---
+++ docs/faq/changing-password.md
@@ -0,0 +1,8 @@
+# How to change your account password
+
+1. Log out of your frii.site account
+2. Go to the login page (www.frii.site/account)
+3. Press "forgot your password?"
+4. Enter your username and send the recovery email
+5. Open the password reset form located in the email that was sent to you
+6. Change your password
